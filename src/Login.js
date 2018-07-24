c4006f1e492a7bffc839d56d9a401b185f736714
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-24 08:19:35 -0500

    create Login component

---
+++ src/Login.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default function Login() {
+  return (
+    <div>
+      <input type="email" name="" id="" placeholder="Phone number, username, or e-mail" />
+    </div>
+  );
+}
