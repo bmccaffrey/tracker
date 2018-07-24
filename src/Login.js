fbf4518775d3e365b40474818784f431788d5b1c
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-24 08:41:50 -0500

    add basic styles to input fields

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 export default function Login() {
   return (
     <div>
-      <input type="email" name="" id="" placeholder="Phone number, username, or e-mail" />
-      <input type="password" name="" id="" placeholder="Password" />
+      <UserInfo type="email" name="" id="" placeholder="Phone number, username, or e-mail" />
+      <UserInfo type="password" name="" id="" placeholder="Password" />
       <button type="submit">
 Login / Creat Account
       </button>
     </div>
   );
 }
+
+const UserInfo = styled.input`
+  width: 100%;
+  height: 30px;
+  text-align: center;
+  background: #eee;
+  color: #444;
+  border-radius: 10px;
+  margin-top: 25px;
+`;
