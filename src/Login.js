b26a9c4c33f8688ee4f08c6f512150a940410e19
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-24 09:05:51 -0500

    refactor styling on input fields

--- src/Login.js
+++ src/Login.js
@@ -3,24 +3,38 @@ import styled from 'styled-components';
 
 export default function Login() {
   return (
-    <div>
-      <UserInfo type="email" name="" id="" placeholder="Phone number, username, or e-mail" />
-      <UserInfo type="password" name="" id="" placeholder="Password" />
-      <button type="submit">
-Login / Creat Account
+    <Fields>
+      <UserInfo
+        type="email"
+        name=""
+        id=""
+        placeholder="Phone number, username, or e-mail"
+        className="User"
+      />
+      <UserInfo type="password" name="" id="" placeholder="Password" className="User" />
+      <button type="submit" className="">
+        Login / Creat Account
       </button>
-    </div>
+    </Fields>
   );
 }
 
-const UserInfo = styled.input`
-  background: #eee;
-  border: 0;
-  border-radius: 10px;
-  color: #444;
-  height: 30px;
-  margin-top: 25px;
-  padding: 0;
-  text-align: center;
-  width: 100%;
+const UserInfo = styled.input``;
+
+const Fields = styled.div`
+  background: white;
+  .User {
+    background: #eee;
+    border: 0;
+    border-radius: 10px;
+    color: #444;
+    height: 30px;
+    margin-top: 25px;
+    padding: 0;
+    text-align: center;
+    width: 100%;
+    ::-webkit-input-placeholder {
+      color: #444;
+    }
+  }
 `;
