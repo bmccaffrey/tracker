c4ba494024a9d1e8d53ab53306a59f13f6e1f174
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-25 14:06:23 -0500

    create App display grid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import styled from 'styled-components';
 import Header from './Header';
 import Login from './Login';
 
 const App = () => (
-  <div className="App">
+  <AppGrid className="App">
     <Header />
     <Login />
-  </div>
+  </AppGrid>
 );
 
 export default App;
+
+const AppGrid = styled.div`
+  display: grid;
+  grid-template-rows: repeat(2, 25%);
+`;
