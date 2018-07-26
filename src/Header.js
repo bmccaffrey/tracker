e539decd5f8f5413e0fe2b39b6352f94dc7b108a
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-26 15:26:10 -0500

    trim import modules

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import {
-  BrowserRouter as Router, Route, Switch, Link,
-} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 
 const Header = () => (
