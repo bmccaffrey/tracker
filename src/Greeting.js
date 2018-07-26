0f4b9add1099feeaed51d61172efd133f97f3fa3
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-26 18:51:17 -0500

    set state

--- src/Greeting.js
+++ src/Greeting.js
@@ -4,28 +4,35 @@ import Header from './Header';
 class Greeting extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      value: '',
+    };
   }
-
-  submit = () => {
-    console.log(this.text.value);
-  };
 
   render() {
     return (
       <div>
         <Header />
-        <h1>Hiya!</h1>
+        <h1>
+Hiya!
+        </h1>
 
-        <h2>Let's get tracking!</h2>
+        <h2>
+Let's get tracking!
+        </h2>
         <hr />
-        <p>What would you like to start tracking?</p>
+        <p>
+What would you like to start tracking?
+        </p>
 
-        <input type="text" ref={input => (this.text = input)} />
+        <input type="text" />
         <button type="submit" onClick={this.submit}>
           Track
         </button>
 
-        <button>Clear</button>
+        <button>
+Clear
+        </button>
       </div>
     );
   }
