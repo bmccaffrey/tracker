1b36efaaf2e629d815399ea1e3375da0b71ea608
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-26 15:26:43 -0500

    correct Link import

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Login = () => (
@@ -15,7 +15,6 @@ const Login = () => (
     <Forget>
 Forgot password?
     </Forget>
-
     <Link to="/greeting">
       <button type="submit" className="User">
         Login / Creat Account
