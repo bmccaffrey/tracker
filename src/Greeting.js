0e43375370d9049443f736a77c8dd3b6f0e23024
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-26 18:31:25 -0500

    make greeting a class component

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 
-const Greeting = () => (
-  <div>
-    <Header />
-    <h1>
+class Greeting extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>
 Hiya!
-    </h1>
-    <h2>
+        </h1>
+
+        <h2>
 Let's get tracking!
-    </h2>
-    <hr />
-    <p>
+        </h2>
+        <hr />
+        <p>
 What would you like to start tracking?
-    </p>
-    <input type="text" />
-    <button>
+        </p>
+
+        <input type="text" />
+        <button type="submit">
 Track
-    </button>
-    <button>
+        </button>
+
+        <button>
 Clear
-    </button>
-  </div>
-);
+        </button>
+      </div>
+    );
+  }
+}
 export default Greeting;
