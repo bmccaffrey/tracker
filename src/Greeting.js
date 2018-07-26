79516220e25f85918227e6a5959521449339630b
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-26 18:32:34 -0500

    add class constructor

--- src/Greeting.js
+++ src/Greeting.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 class Greeting extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div>
