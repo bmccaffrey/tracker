32f84c10e656a2b759da5f9b6a348c0add6e924e
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-26 18:53:44 -0500

    define funtion to set state according to input

--- src/Greeting.js
+++ src/Greeting.js
@@ -9,30 +9,27 @@ class Greeting extends Component {
     };
   }
 
+  handleInput = e => {
+    this.setState({ value: e.target.value });
+    console.log(this.state.value);
+  };
+
   render() {
     return (
       <div>
         <Header />
-        <h1>
-Hiya!
-        </h1>
+        <h1>Hiya!</h1>
 
-        <h2>
-Let's get tracking!
-        </h2>
+        <h2>Let's get tracking!</h2>
         <hr />
-        <p>
-What would you like to start tracking?
-        </p>
+        <p>What would you like to start tracking?</p>
 
-        <input type="text" value={this.state.value} />
+        <input type="text" value={this.state.value} onInput={this.handleInput} />
         <button type="submit" onClick={this.submit}>
           Track
         </button>
 
-        <button>
-Clear
-        </button>
+        <button>Clear</button>
       </div>
     );
   }
