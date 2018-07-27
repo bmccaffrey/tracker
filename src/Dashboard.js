33710182a547d06f97ffe57f3f7759fbf043352e
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-26 22:09:22 -0500

    add avatar image

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Avatar from './Avatar.svg';
 
 const Dashboard = () => (
-  <h1>
-Dashboard
-  </h1>
+  <div>
+    <img src={Avatar} alt="" />
+    <h1>
+Welcome to your Dashboard!
+    </h1>
+  </div>
 );
 
 export default Dashboard;
