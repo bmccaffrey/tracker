153dbc79b3a68da1f7535b3614a1122b5b6ceba9
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-27 06:28:29 -0500

    add introductory text to Analytics

--- src/Analytics.js
+++ src/Analytics.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Analytics = () => (
   <div>
-Test
+    <h1>
+      Let&apos;s analyze
+      <br />
+      &amp; compare
+    </h1>
+    <h2>
+and see if we can&apos;t come up with some insights...
+    </h2>
   </div>
 );
 
