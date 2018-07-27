ca6589a66427a1ac460823b8187903da84fb6d2c
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-27 04:05:15 -0500

    adjust menu icon positioning

--- src/Hamburger.js
+++ src/Hamburger.js
@@ -13,6 +13,9 @@ export default Hamburger;
 
 const Burger = styled.div`
   height: 24px;
+  position: relative;
+  bottom: 5px;
+  right: 10px;
   > div {
     border: 1px solid #fff;
     width: 18px;
