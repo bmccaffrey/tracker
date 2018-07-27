2ef63ac4d7c964559ea1651718d6698651617f65
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-27 04:29:50 -0500

    set placeholder text

--- src/Tracking.js
+++ src/Tracking.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Tracking = () => (
-
+  <div>
+    <h1>
+Alright!
+    </h1>
+    <h2>
+We're currently tracking:
+    </h2>
+  </div>
 );
 
 export default Tracking;
