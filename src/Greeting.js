cd40c7ff9f5e412c31df6e40a38bf7671bdb3c42
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-26 19:42:12 -0500

    add tracking message to state

--- src/Greeting.js
+++ src/Greeting.js
@@ -6,6 +6,14 @@ class Greeting extends Component {
     super(props);
     this.state = {
       value: '',
+      message: (
+        <div>
+          <h1>Hiya!</h1>
+          <h2>Let's get tracking!</h2>
+          <hr />
+          <p>What would you like to start tracking?</p>
+        </div>
+      ),
     };
   }
 
