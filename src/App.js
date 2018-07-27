165dd75e918e7aede52a9b23fbd59d2279b7b4af
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-26 21:34:54 -0500

    set route for Dashboard

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 import LoginPage from './LoginPage';
 import Greeting from './Greeting';
 import Header from './Header';
+import Dashboard from './Dashboard';
 
 const App = () => (
   <Router>
@@ -13,6 +14,7 @@ const App = () => (
       <Header />
       <Route exact path="/" component={LoginPage} />
       <Route path="/greeting" component={Greeting} />
+      <Route path="/dashboard" component={Dashboard} />
     </div>
   </Router>
 );
