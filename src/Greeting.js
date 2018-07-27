09ba64d50cbcc10cb5ab124eb3ee87b42b907cf5
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-26 19:13:09 -0500

    conditionally render tracking messages

--- src/Greeting.js
+++ src/Greeting.js
@@ -15,15 +15,28 @@ class Greeting extends Component {
   };
 
   render() {
+    let message;
+    if (this.state.value) {
+      message = (
+        <div>
+          <h1>Track my {this.state.value}</h1>
+        </div>
+      );
+    } else {
+      message = (
+        <div>
+          <h1>Hiya!</h1>
+          <h2>Let's get tracking!</h2>
+          <hr />
+          <p>What would you like to start tracking?</p>
+        </div>
+      );
+    }
+
     return (
       <div>
         <Header />
-        <h1>Hiya!</h1>
-
-        <h2>Let's get tracking!</h2>
-        <hr />
-        <p>What would you like to start tracking?</p>
-
+        {message}
         <input type="text" value={this.state.value} onInput={this.handleInput} />
         <button type="submit" onClick={this.submit}>
           Track
