d7974fd0b5b70e702ea409627664ecbae106e441
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-31 00:26:13 -0500

    add class on state change

--- src/Hamburger.js
+++ src/Hamburger.js
@@ -1,58 +1,44 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import StyledLink from './StyledLink';
+import './Hamburger.css';
 
 export default class Hamburger extends Component {
   constructor(props) {
     super(props);
     this.state = {
       on: false,
-      class: '',
+      class: 'container',
     };
   }
 
   toggle = () => {
     this.setState({
       on: !this.state.on,
-      class: 'line1',
     });
-    if (this.state.on) {
-      this.setState({ class: 'line1' });
-    } else {
-      this.setState({
-        class: '',
-      });
-    }
+    // if (this.state.on) {
+    //   this.setState({ class: 'container change' });
+    // } else {
+    //   this.setState({
+    //     class: 'container',
+    //   });
+    // }
   };
 
   render() {
+    let className = 'container';
+    if (this.state.on) {
+      className += ' change';
+    }
+
     return (
       <StyledLink to="/dashboard">
-        <Burger onClick={this.toggle}>
-          <div className={this.state.class} />
-          <div className={this.state.class} />
-          <div className={this.state.class} />
-        </Burger>
+        <div onClick={this.toggle} className={className}>
+          <div className="line1" />
+          <div className="line2" />
+          <div className="line3" />
+        </div>
       </StyledLink>
     );
   }
 }
-
-const Burger = styled.div`
-  height: 24px;
-  position: relative;
-  bottom: 5px;
-  right: 10px;
-  > div {
-    border: 1px solid #fff;
-    width: 18px;
-    margin: 6px 0;
-  }
-  .line1 {
-    border-color: orange;
-  }
-  .line2 {
-  }
-  .line3 {
-  }
-`;
