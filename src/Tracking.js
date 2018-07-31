de73fc0c0302a6641006e5c1709bba1fe0fd20fe
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-31 11:38:08 -0500

    add additional comments

--- src/Tracking.js
+++ src/Tracking.js
@@ -7,11 +7,16 @@ import Tracked from './Tracked';
 // TODO: -- create input field to allow user to specify time range
 // FIXME: -- need a way to chart data
 // TODO: -- Chart.js ??
+// FIXME: -- Tracked.js could be conflated here
+
+// iterate over localStorage and push objects to items array
 const items = [];
 for (let i = 0; i < localStorage.length; i++) {
   items.push(localStorage[`key${i}`]);
 }
 
+// pass each storage object to Tracked's props.name
+// Tracked returns a styled div for each object
 const Tracking = () => (
   <div>
     <h1>Alright!</h1>
