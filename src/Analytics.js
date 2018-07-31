b93bca7aa2d57280760b6cf60d9d9882fa5c54ed
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-30 21:04:29 -0500

    offset introductory text

--- src/Analytics.js
+++ src/Analytics.js
@@ -1,13 +1,22 @@
 /* eslint react/jsx-one-expression-per-line: 0 */
 import React from 'react';
+import styled from 'styled-components';
 
 const Analytics = () => (
   <div>
-    <h1>
+    {/* <h1>
       Let&apos;s analyze<br />&amp; compare
-    </h1>
+    </h1> */}
+    <h1 style={{ marginBottom: '0px' }}>Let&apos;s analyze</h1>
+    <Intro>&amp; compare</Intro>
     <h2>and see if we can&apos;t come up with some insights...</h2>
   </div>
 );
 
 export default Analytics;
+
+const Intro = styled.div`
+  text-indent: 10%;
+  font-size: 2em;
+  font-weight: bold;
+`;
