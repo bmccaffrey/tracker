7a7e5fe0fd1162842bdae22270ee050d5501b625
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-31 12:35:21 -0500

    display currently tracked list

--- src/Analytics.js
+++ src/Analytics.js
@@ -1,8 +1,9 @@
 /* eslint react/jsx-one-expression-per-line: 0 */
 import React from 'react';
 import styled from 'styled-components';
+import { items } from './Tracking';
+import Tracked from './Tracked';
 
-// TODO: -- display currently tracked
 // FIXME: -- allow user to select which ones to compare
 // FIXME: -- allow user to specify time range
 const Analytics = () => (
@@ -10,6 +11,7 @@ const Analytics = () => (
     <h1 style={{ marginBottom: '0px' }}>Let&apos;s analyze</h1>
     <StyledH1>&amp; compare</StyledH1>
     <h2>and see if we can&apos;t come up with some insights...</h2>
+    {items.map(item => <Tracked name={item} />)}
   </Container>
 );
 
