dad02e1d02bf6a823e775981cceda4aa188da67f
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-30 20:48:09 -0500

    add styling

--- src/Tracked.js
+++ src/Tracked.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Tracked = props => (
-  <div>
+  <Container>
     {props.name}
-  </div>
+  </Container>
 );
 
 export default Tracked;
+
+const Container = styled.div`
+  background: #eee;
+  border: 0;
+  border-radius: 10px;
+  color: #444;
+  height: 30px;
+  line-height: 30px;
+  margin-top: 25px;
+  padding: 0;
+  text-align: center;
+  vertical-align: middle;
+`;
