a1b090d4d21dab54ff8e6bd2f4788b472324b991
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-30 20:26:43 -0500

    import Tracked and pass items to props.name

--- src/Tracking.js
+++ src/Tracking.js
@@ -1,5 +1,6 @@
 /* eslint react/jsx-one-expression-per-line: 0 */
 import React from 'react';
+import Tracked from './Tracked';
 
 const items = [];
 for (let i = 0; i < localStorage.length; i++) {
@@ -10,7 +11,7 @@ const Tracking = () => (
   <div>
     <h1>Alright!</h1>
     <h2>We're currently tracking your:</h2>
-    {items.map(item => <h3>{item}</h3>)}
+    {items.map(item => <Tracked name={item} />)}
   </div>
 );
 
