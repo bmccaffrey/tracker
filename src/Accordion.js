a24e1e68505181148d2645bb20874f63122bf4d9
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-31 06:09:28 -0500

    create Accordion component

---
+++ src/Accordion.js
@@ -0,0 +1,21 @@
+/* eslint react/jsx-one-expression-per-line: 0 */
+/* eslint react/button-has-type: 0 */
+import React, { Component } from 'react';
+import Toggle from './Toggle';
+
+const Accordion = (props) => {
+  const { children } = props;
+  const title = children[0];
+  const Hide = children.slice(1);
+  return (
+    <Toggle>
+      {({ on, toggle }) => (
+        <div>
+          <p onClick={toggle}>{title}</p>
+          {on && <p>{Hide}</p>}
+        </div>
+      )}
+    </Toggle>
+  );
+};
+export default Accordion;
