b57c4d2de1a457d9c9f3687d528fa547ced812f8
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-07-30 20:15:43 -0500

    iterate through locally stored items

--- src/Tracking.js
+++ src/Tracking.js
@@ -1,6 +1,11 @@
 /* eslint react/jsx-one-expression-per-line: 0 */
 import React from 'react';
 
+const items = [];
+for (let i = 0; i < localStorage.length; i++) {
+  items.push(localStorage[`key${i}`]);
+}
+
 const Tracking = () => (
   <div>
     <h1>Alright!</h1>
