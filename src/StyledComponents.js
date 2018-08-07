b897ec805ebef5590e781cdd0d76522160e795e8
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-07 18:24:39 -0500

    add StyledInput

--- src/StyledComponents.js
+++ src/StyledComponents.js
@@ -4,4 +4,17 @@ import styled from 'styled-components';
 const MarginContainer = styled.div`
   margin: 5%;
 `;
+
+const StyledInput = styled.input`
+  background: #eee;
+  border: 0;
+  border-radius: 10px;
+  color: #444;
+  height: 30px;
+  width: 100%;
+  line-height: 30px;
+  padding: 0;
+`;
+
 export { MarginContainer };
+export { StyledInput };
