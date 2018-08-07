a6f7c2d8b4901b3244588d0bd30dc555fc47af19
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-07 18:24:57 -0500

    apply styles

--- src/Simplified.js
+++ src/Simplified.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+import { MarginContainer, StyledInput } from './StyledComponents';
 
 const Simplified = () => (
-  <div>
+  <MarginContainer>
     <h1>
 What would you like to track?
     </h1>
-    <input type="text" />
+    <StyledInput type="text" />
     <h1>
 How would you like to track it?
     </h1>
-    <input type="text" />
+    <StyledInput type="text" />
     <h1>
 When's good for you?
     </h1>
-    <input type="text" />
+    <StyledInput type="text" />
     <h1>
 Why do you want to track it?
     </h1>
-    <input type="text" />
-  </div>
+    <StyledInput type="text" />
+  </MarginContainer>
 );
 export default Simplified;
