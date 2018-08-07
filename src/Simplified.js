e11b9cefb36ee063fc28a32d8b5dd0571bf8217c
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-07 17:53:35 -0500

    create simplified new item page

---
+++ src/Simplified.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Simplified = () => (
+  <div>
+    <h1>
+Placeholder Text
+    </h1>
+    <input type="text" />
+    <h1>
+Placeholder Text
+    </h1>
+    <input type="text" />
+    <h1>
+Placeholder Text
+    </h1>
+    <input type="text" />
+    <h1>
+Placeholder Text
+    </h1>
+    <input type="text" />
+  </div>
+);
+export default Simplified;
