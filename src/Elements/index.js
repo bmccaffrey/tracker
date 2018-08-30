eb63fa7573914e6478d4d8006b1ce8daaa3c5abf
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 03:26:05 -0500

    add Header to index

--- src/Elements/index.js
+++ src/Elements/index.js
@@ -5,8 +5,9 @@ import Greeting from './Greeting';
 import Dashboard from './Dashboard';
 import Tracking from './Tracking';
 import Analytics from './Analytics';
+import Header from './Header';
 import Hamburger from './Hamburger';
 
 export {
-  StyledLink, Theme, Login, Greeting, Dashboard, Tracking, Analytics, Hamburger,
+  StyledLink, Theme, Login, Greeting, Dashboard, Tracking, Analytics, Header, Hamburger,
 };
