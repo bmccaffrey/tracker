87a036cb1eea9e2a266daf6cc31aa8750f4379f7
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 02:41:09 -0500

    correct import statements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Login } from './Elements';
+import { Login, Analytics } from './Elements';
 import Greeting from './Greeting';
 import Header from './Header';
 import Dashboard from './Dashboard';
 import Tracking from './Tracking';
-import Analytics from './Analytics';
 
 // TODO: -- import routes & components from Dashboard.js
 // FIXME: -- can't access certains routes w/o being logged in
