551dc863856983c35c196ec477e66759e8fdf945
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 02:44:29 -0500

    add Greeting to index

--- src/Elements/index.js
+++ src/Elements/index.js
@@ -1,8 +1,9 @@
 import StyledLink from './StyledLink';
 import Theme from './Theme';
 import Login from './Login';
+import Greeting from './Greeting';
 import Analytics from './Analytics';
 
 export {
-  StyledLink, Theme, Login, Analytics,
+  StyledLink, Theme, Login, Greeting, Analytics,
 };
