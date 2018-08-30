ad855793d6a02e89cfabad4753d6bb9ff7ccf3da
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 04:19:30 -0500

    add element to index

--- src/Elements/index.js
+++ src/Elements/index.js
@@ -1,5 +1,8 @@
 import StyledLink from './StyledLink';
 import Theme from './Theme';
 import Input from './Input';
+import StyledButton from './StyledButton';
 
-export { StyledLink, Theme, Input };
+export {
+  StyledLink, Theme, Input, StyledButton,
+};
