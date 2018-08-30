4012a349d7ba8fb6ffcf8bf6ceed9141553c6d8c
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 04:19:11 -0500

    create Styled Button element

---
+++ src/Elements/StyledButton.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: grey;
+  color: white;
+  margin-top: 15px;
+  margin-right: 5px;
+  height: 30px;
+  width: 75px;
+  border-radius: 10px;
+`;
+
+export default StyledButton;
