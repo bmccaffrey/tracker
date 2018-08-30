d1631c8e1bbb7cca182cdb367d84fe392ac1338c
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 05:22:35 -0500

    add component to index

--- src/Components/index.js
+++ src/Components/index.js
@@ -5,7 +5,8 @@ import Tracking from './Tracking';
 import Analytics from './Analytics';
 import Header from './Header';
 import Hamburger from './Hamburger';
+import Simplified from './Simplified';
 
 export {
-  Login, Greeting, Dashboard, Tracking, Analytics, Header, Hamburger,
+  Login, Greeting, Dashboard, Tracking, Analytics, Header, Hamburger, Simplified,
 };
