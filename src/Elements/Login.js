108cfcc7e75140ae06d4035ebe318bbf97e4d23e
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 01:45:00 -0500

    move Login to Elements library

---
+++ src/Elements/Login.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import StyledLink from './StyledLink';
+import Theme from './Theme';
+
+// TODO: -- store username in localStorage
+// TODO: -- store password in localStorage
+// TODO: -- set prop for LoggedIn
+// TODO: -- create a PasswordReset Component
+const Login = () => (
+  <AppGrid>
+    <div />
+    <div>
+      <Input type="email" name="" id="" placeholder="Phone number, username, or e-mail" />
+      <Input type="password" name="" id="" placeholder="Password" />
+      <Forget>
+Forgot password?
+      </Forget>
+      <StyledLink to="/greeting">
+        <Button type="submit">
+Login / Creat Account
+        </Button>
+      </StyledLink>
+    </div>
+  </AppGrid>
+);
+
+export default Login;
+
+const Placeholder = Theme.extend`
+  width: 100%;
+  text-align: center;
+  ::-webkit-input-placeholder {
+    color: #444;
+  }
+`;
+const Input = Placeholder.withComponent('input');
+const Button = Placeholder.withComponent('button');
+
+const Forget = styled.div`
+  font-size: 0.75rem;
+  text-align: right;
+  margin-top: 10px;
+`;
+
+const AppGrid = styled.div`
+  display: grid;
+  grid-template-rows: repeat(2, 25%);
+  height: 100vh;
+`;
