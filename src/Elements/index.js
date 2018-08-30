bcc918d279ca5f0769ca4c7f8b4516026178e2bf
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 03:19:36 -0500

    add Tracking to index

--- src/Elements/index.js
+++ src/Elements/index.js
@@ -3,8 +3,9 @@ import Theme from './Theme';
 import Login from './Login';
 import Greeting from './Greeting';
 import Dashboard from './Dashboard';
+import Tracking from './Tracking';
 import Analytics from './Analytics';
 
 export {
-  StyledLink, Theme, Login, Greeting, Analytics, Dashboard,
+  StyledLink, Theme, Login, Greeting, Dashboard, Tracking, Analytics,
 };
