3fd498897e7f928b9c2353d2bfd1339b061fd57b
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 03:22:00 -0500

    add Hamburger to index

--- src/Elements/index.js
+++ src/Elements/index.js
@@ -5,7 +5,8 @@ import Greeting from './Greeting';
 import Dashboard from './Dashboard';
 import Tracking from './Tracking';
 import Analytics from './Analytics';
+import Hamburger from './Hamburger';
 
 export {
-  StyledLink, Theme, Login, Greeting, Dashboard, Tracking, Analytics,
+  StyledLink, Theme, Login, Greeting, Dashboard, Tracking, Analytics, Hamburger,
 };
