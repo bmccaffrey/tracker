8977ab836bc672af7600ff259637abb51fd10333
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 03:43:33 -0500

    remove from index

--- src/Elements/index.js
+++ src/Elements/index.js
@@ -1,13 +1,4 @@
 import StyledLink from './StyledLink';
 import Theme from './Theme';
-import Login from './Login';
-import Greeting from './Greeting';
-import Dashboard from './Dashboard';
-import Tracking from './Tracking';
-import Analytics from './Analytics';
-import Header from './Header';
-import Hamburger from './Hamburger';
 
-export {
-  StyledLink, Theme, Login, Greeting, Dashboard, Tracking, Analytics, Header, Hamburger,
-};
+export { StyledLink, Theme };
