9ef7b002b5d5e4f55bef942c478cae48f16d3d47
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-08-30 05:21:38 -0500

    add simplified tracking component

---
+++ src/Components/Simplified.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MarginContainer = styled.div`
+  margin: 5%;
+`;
+
+const StyledInput = styled.input`
+  background: #eee;
+  border: 0;
+  border-radius: 10px;
+  color: #444;
+  height: 30px;
+  width: 100%;
+  line-height: 30px;
+  padding: 0;
+`;
+
+const Simplified = () => (
+  <MarginContainer>
+    <h1>
+What would you like to track?
+    </h1>
+    <StyledInput type="text" />
+    <h1>
+How would you like to track it?
+    </h1>
+    <StyledInput type="text" />
+    <h1>
+When's good for you?
+    </h1>
+    <StyledInput type="text" />
+    <h1>
+Why do you want to track it?
+    </h1>
+    <StyledInput type="text" />
+  </MarginContainer>
+);
+export default Simplified;
