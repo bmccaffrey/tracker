c644c8be61f5f8024aa8b53143bdd8ae54ca56c5
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-23 15:48:39 -0600

    add throwaway to comp exporter index

--- src/Components/index.js
+++ src/Components/index.js
@@ -6,7 +6,16 @@ import Analytics from './Analytics';
 import Header from './Header';
 import Hamburger from './Hamburger';
 import Simplified from './Simplified';
+import API_FORM_TESTER from './Throwaway';
 
 export {
-  Login, Greeting, Dashboard, Tracking, Analytics, Header, Hamburger, Simplified,
+  Login,
+  Greeting,
+  Dashboard,
+  Tracking,
+  Analytics,
+  Header,
+  Hamburger,
+  Simplified,
+  API_FORM_TESTER,
 };
