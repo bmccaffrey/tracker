5462847065b2c657a5a0656dea81eb08033d23c8
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-23 14:36:57 -0600

    simplify GET display comp

--- src/Components/GetExample.js
+++ src/Components/GetExample.js
@@ -5,35 +5,31 @@ export default class GetExample extends Component {
     super(props);
     this.state = {
       loading: true,
-      recipes: [],
+      examples: [],
     };
   }
 
   componentDidMount() {
-    this.getRecipes();
+    this.getExamples();
   }
 
-  get = route => {
-    const response = fetch(route)
+  fetcher = (route, method) => {
+    const response = fetch(route, { method })
       .then(body => body.text())
       .then(text => JSON.parse(text))
       .catch(err => console.log(err));
     return response;
   };
 
-  async getRecipes() {
-    this.setState({ loading: false, recipes: await this.get('/recipes') });
+  async getExamples() {
+    this.setState({ loading: false, examples: await this.fetcher('/all') });
   }
 
   render() {
-    const { recipes } = this.state;
-    const { name, ingredients, directions } = recipes;
+    // let name, metric, freq, why, tracks;
+    const { examples } = this.state;
     return (
-      <div>
-        <h2>{name}</h2>
-        <h3>{ingredients}</h3>
-        <h4>{directions}</h4>
-      </div>
+      <div>{examples.length ? examples.map(elm => <h2>{elm.name}</h2>) : <h3>Loading</h3>}</div>
     );
   }
 }
