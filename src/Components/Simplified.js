d763867e38b731a379e8ad04fc4e7736041ac71f
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-12-24 05:04:52 -0600

    Add functionality to new activity form

--- src/Components/Simplified.js
+++ src/Components/Simplified.js
@@ -16,18 +16,20 @@ const StyledInput = styled.input`
   line-height: 30px;
   padding: 0;
 `;
-
+// TODO: Add a Success message/indicator
 const Simplified = () => (
   <MarginContainer>
-    <h1>What would you like to track?</h1>
-    <StyledInput type="text" />
-    <h1>How would you like to track it?</h1>
-    <StyledInput type="text" />
-    <h1>When's good for you?</h1>
-    <StyledInput type="text" />
-    <h1>Why do you want to track it?</h1>
-    <StyledInput type="text" />
-    <GetExample />
+    <form name="activity-adder" action="/addactivity" method="post">
+      <h1>What would you like to track?</h1>
+      <StyledInput type="text" name="name" id="name-input" />
+      <h1>How would you like to track it?</h1>
+      <StyledInput type="text" name="metric" id="metric-input" />
+      <h1>When's good for you?</h1>
+      <StyledInput type="text" name="freq" id="freq-input" />
+      <h1>Why do you want to track it?</h1>
+      <StyledInput type="text" name="why" id="why-input" />
+      <input type="submit" value="Save" />
+    </form>
   </MarginContainer>
 );
 export default Simplified;
